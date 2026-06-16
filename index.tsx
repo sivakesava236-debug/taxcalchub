0bc4854213b0072ce3137647856057d1e2bdd702
sivakesava236-debug <sivakesava236@gmail.com>
2026-06-16 16:47:03 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  beforeLoad: () => {
+    throw redirect({ href: "/taxora.html" });
+  },
+  component: () => null,
+});
